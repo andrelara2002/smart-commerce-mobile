4b6bc94e458502f19fe51d0219db15eb5d2de431
André Lara <66387919+andrelara2002@users.noreply.github.com>
2021-08-23 22:06:04 -0300

    Branch

--- src/pages/Login/LoginView.js
+++ src/pages/Login/LoginView.js
@@ -8,6 +8,7 @@ import DefaultColors from '../../res/colors/DefaultColors';
 export default function LoginView(props) {
 
     const texts = LoginTexts()[props.lang];
+    console.log("Branch André")
     const styles = StyleSheet.create({
         container: {
             flex: 1,
