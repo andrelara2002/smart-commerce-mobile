d82b69e8c1476021fd75014746de9630a462642a
André Lara <66387919+andrelara2002@users.noreply.github.com>
2021-09-06 20:03:49 -0300

    Concluindo mais votados

--- src/Components/Graphs/HorBarGraph/HotBarGraph.js
+++ src/Components/Graphs/HorBarGraph/HotBarGraph.js
@@ -6,7 +6,7 @@ import DefaultColors from '../../../assets/colors/DefaultColors';
 
 export default function HotBarGraph({ image, qtdTotalVotos, qtdVotos, index, title }) {
 
-    const { accent } = DefaultColors["dark"];
+    const { accent, textColor, background } = DefaultColors["dark"];
 
     const [
         porcentagem,
@@ -19,18 +19,60 @@ export default function HotBarGraph({ image, qtdTotalVotos, qtdVotos, index, tit
     const styles = StyleSheet.create({
         container: {
             flexDirection: 'row',
+            alignItems: 'center',
             marginBottom: 10,
             marginTop: 10,
+            paddingLeft: 10,
+            paddingRight: 10,
+        },
+        title: {
+            fontSize: 14,
+            fontWeight: 'bold',
+            color: textColor,
+            marginBottom: 5,
+        },
+        position: {
+            color: accent,
+            fontSize: 16,
+        },
+        item: {
+            marginLeft: 10,
+            width: '80%',
+        },
+        progressbar: {
+            height: 10,
+            backgroundColor: background,
+            borderRadius: 10,
+        },
+        image: {
+            width: 20,
+            height: 20,
+            marginRight: 10,
+            borderRadius: 10,
+        },
+        texts: {
+            justifyContent: 'space-between',
+            flexDirection: 'row',
+            padding: 0,
+            margin: 0
+        },
+        percent: {
+            fontSize: 12,
+            color: accent,
         }
     })
 
     return (
         <View style={styles.container}>
-            <Image />
-            <Text>{index + 1}</Text>
-            <View>
-                <Text>{title}</Text>
+            <Image source={image} style={styles.image} />
+            <Text style={styles.position}>{index + 1}</Text>
+            <View style={styles.item}>
+                <View style={styles.texts}>
+                    <Text style={styles.title}>{title}</Text>
+                    <Text style={styles.percent}>{`${qtdVotos} Votos`}</Text>
+                </View>
                 <ProgressBar
+                    style={styles.progressbar}
                     progress={porcentagem}
                     color={accent} />
             </View>
--- src/Components/Graphs/HorBarGraph/MostVotted.js
+++ src/Components/Graphs/HorBarGraph/MostVotted.js
@@ -5,30 +5,54 @@ import { View, FlatList, Text, StyleSheet } from 'react-native'
 import HotBarGraph from './HotBarGraph'
 import { mostVotted } from '../../../pages/Home/HomeCarrocel/FakeData'
 
+import DefaultColors from '../../../assets/colors/DefaultColors'
+
 export default function MostVotted(props) {
 
     const data = mostVotted
 
+    const { accent, textColor, backgroundSecondary } = DefaultColors["dark"]
+
     const styles = StyleSheet.create({
         container: {
             padding: 20,
+        },
+        title: {
+            fontSize: 20,
+            fontWeight: 'bold',
+            marginBottom: 10,
+            color: textColor,
+        },
+        flatList: {
+            backgroundColor: backgroundSecondary,
+            padding: 10,
+            borderRadius: 10,
+            width: '98%',
         }
     })
 
+    function renderItens() {
+        return data.map((item, index) => {
+            return (
+                <HotBarGraph
+                    key={index}
+                    image={item.image}
+                    title={item.title}
+                    qtdVotos={item.qtdVotos}
+                    qtdTotalVotos={item.qtdTotalVotos}
+                    index={index}
+                />
+            )
+        }
+        )
+    }
+
     return (
         <View style={styles.container}>
-            <Text>Most Votted</Text>
-            <FlatList
-                data={data}
-                renderItem={({ item, index }) =>
-                    <HotBarGraph
-                        image={item.image}
-                        title={item.title}
-                        qtdVotos={item.qtdVotos}
-                        qtdTotalVotos={item.qtdTotalVotos}
-                        index={index}
-                    />}
-            />
+            <Text style={styles.title}>Most Votted</Text>
+            <View style={styles.flatList}>
+                {renderItens()}
+            </View>
         </View>
     )
 }
