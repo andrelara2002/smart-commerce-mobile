579b1728a4bdcbcc28fcdfe88b8ab41b5808fe7c
André Lara <66387919+andrelara2002@users.noreply.github.com>
2021-10-21 20:55:21 -0300

    Update DefaultColors.js

--- src/assets/colors/DefaultColors.js
+++ src/assets/colors/DefaultColors.js
@@ -10,5 +10,15 @@ export default DefaultColors = {
         textColor: "#ffffff",
         red: "#FF4B4B",
     },
-    "light": {}
+    "light": {
+        background: "#ffffff",
+        backgroundSecondary: "#F7F7F7",
+        accent: "#3F8CFF",
+        sec_accent: "#62A1FF",
+        trd_accent: "#82B4FF",
+        border: "#E5E5E5",
+        deactivate: "#E5E5E5",
+        textColor: "#000000",
+        red: "#FF4B4B",
+    }
 }
